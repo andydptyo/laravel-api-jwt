d39564d0e03ae4870560db4fac5d2a34d2627611
user <andy.dptyo@gmail.com>
2019-07-29 15:34:06 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\User;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('users', 'UserController')->middleware('jwt.verify');
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
